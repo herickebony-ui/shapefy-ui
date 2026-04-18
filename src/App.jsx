ff86ad0668630d3113e288d7b3a257480c9dde1c
Hérick Ébony <herickebony@Hericks-MacBook-Air-2.local>
2026-04-18 13:26:04 -0300

    Revert "feat: componentes migrados do onboarding-consultoria"
    This reverts commit f0953c20bc04b6364028b3b60b024b073dd0f0a7.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import useAuthStore from './store/authStore'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import HubAlunos from './pages/Alunos/HubAlunos'
-import AppLayout from './components/layout/AppLayout'
 
 function PrivateRoute({ children }) {
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated)
@@ -15,12 +13,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={
-          <PrivateRoute><AppLayout /></PrivateRoute>
-        }>
-          <Route index element={<Dashboard />} />
-          <Route path="alunos" element={<HubAlunos />} />
-        </Route>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
